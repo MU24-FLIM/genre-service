59efca5be1dcd6c0d3d92a27aca599643b53004a
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2025-03-27 13:37:14 +0100

    added prepered genre

---
+++ src/main/resources/genredata.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO genre (name, description) VALUES
+('Action', 'Heart-racing thrills, epic fights, and daring stunts—nonstop chaos with heroes who never back down!'),
+('Sci-Fi', 'Blasting you to the future with mind-bending tech, alien adventures, and worlds that make your imagination do flips!'),
+('Comedy', 'Packed with crazy situations, witty one-liners, and jokes that hit you when you least expect it—pure laugh-out-loud fun!'),
+('Romance', 'Filled with love, heart-fluttering moments, and swoon-worthy connections, where every glance could spark a happily ever after!'),
+('Fantasy', 'Transporting you to magical realms with epic quests, mythical creatures, and worlds where anything is possible!')
+;
